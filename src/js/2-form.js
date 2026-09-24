0e4578b76c008ba6257a78c5e7b697d78345f8c8
alexbublov <twinkg33@gmail.com>
2026-09-24 22:49:30 +0300

    Add hw files

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.feedback-form');
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+const fillFormData = () => {
+    const savedData = localStorage.getItem('feedback-form-state');
+
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        formData = parsedData;
+
+        Object.keys(parsedData).forEach(key => {
+            form.elements[key].value = parsedData[key];
+        })
+    }
+};
+fillFormData();
+
+form.addEventListener('change', ({ target: formFieldEl }) => {
+    formData[formFieldEl.name] = formFieldEl.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (Object.values(formData).includes('')) {
+        alert('Please fill in all fields before submitting the form.');
+        return;
+    }
+
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+});
